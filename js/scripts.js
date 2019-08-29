445a76ac50e0258c6d2aea4c1f20c3b4b50b54ab
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-08-29 11:54:53 -0700

    Completed UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,36 @@ function Player(turn) {
   this.playername;
 
 }
+// Checking for 1 -----
+Player.prototype.rollone = function(){
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert ("Sorry" + this.playername + " , end of your turn, next players turn!");
+  }
+}
+  // hold ------
+Player. prototype.hold = function(){
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  alert(this.playerName + ", next players turn");
+
+}
+
+// Checking for a winner ----
+Player .prototype.winnerCheck = function(){
+  if(this.total >= 100){
+    alert(this.playerName + ", You have won!!!! Congrates on being the winner winner for this game!");
+  }
+}
+// new Game ------------
+Player.prototype.newGame= function(){
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.playerName ="";
+}
+// Clearing values ---------
+var clearValues= function(){
+  $(".player1Name").val();
+  $(".player2Name").val();
+}
