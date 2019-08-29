048964da9d6a57f6ef9dee6ca15e42c25600df9c
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-08-29 13:51:05 -0700

    updated biz and UI logic
    need to match my ID and classes

--- js/scripts.js
+++ js/scripts.js
@@ -50,3 +50,43 @@ var clearValues= function(){
   $(".player1Name").val();
   $(".player2Name").val();
 }
+
+// User Interface Logic -------
+$(document).ready(function() {
+
+  $("button#start").click(function(event) {
+    player1 = new Player(true);
+     player2 =  new Player(false);
+     $(".player-console").show();
+     $(".start-menu").hide();
+
+    var player1Name = $(".player1Name").val();
+    var player2Name = $(".player2Name").val();
+
+    player1.player1name = player1Name;
+    player2.player2name = player2Name;
+
+  });
+
+  $("button#new-game").click(function(event) {
+    $(".player-console").hide();
+    clearvalues();
+    player1.newGame();
+    player1.newGame();
+    $("#round-total-1").empty();
+    $("#total-score-1").empty();
+    $("#die-role-1").empty();
+    $("#round-total-2").empty();
+    $("#total-score-2").empty();
+    $("#die-role-2").empty();
+
+    $(".start-menue").show();
+  });
+
+  ("button#player1-roll").click(function(event){
+    player1.roll=roleDice();
+    $("#die-roll-1").text(player1.roll);
+    player1.rollone();
+    $("#round-total-1").text(player1.tempscore);
+  });
+});
