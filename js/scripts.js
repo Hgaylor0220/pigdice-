d6e5492fa39d21c223061ec2001f6987c0f98fd5
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-08-29 10:53:18 -0700

    initial commit. Added buttons and started js

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+
+
+var playerOne = function 
+var playerTwo =
+
+var total= 0;
+var role = [2, 3, 4, 5, 6];
+role.forEach(function(role) {
+  total += role;
+});
+alert(total);
+
+
+$(document).ready(function() {
+  $("#pigDice").submit(function(event) {
+    alert("hello");
+function myFunction() {
+  roleDice.addEventListerner ("button")
+  document.getElementById("score").ParseInt = (roleDice);
+   event.preventDefault();
+   console.log(".roleDice");
+    }
+  });
+});
